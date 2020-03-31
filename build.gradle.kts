91ee6b9e586015ae1cce32cc74761ba3998f7e91
Mladen Čikara <mladen.cikara@gmail.com>
2020-03-31 23:58:01 +0200

    Configuring test dependencies for JUnit5

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,13 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
+    testImplementation("org.jetbrains.kotlin:kotlin-test:kotlin-test-junit5")
+    // Use the Kotlin test library.
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
+    // Use the Kotlin JUnit integration.
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.6.1")
+
 }
 
 tasks {
@@ -20,4 +27,8 @@ tasks {
     compileTestKotlin {
         kotlinOptions.jvmTarget = "1.8"
     }
+}
+
+tasks.test {
+    useJUnitPlatform()
 }
